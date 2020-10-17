0bff5170311d1fd34b9d3ebde46b1147daf7ed7c
Surage_Mars <Surage_Mars@host.docker.internal>
2020-10-17 14:49:18 -0700

    first commit

---
+++ Student Database Application/src/student.java
@@ -0,0 +1,91 @@
+import java.util.*;
+public class student {
+	
+	private String firstName;
+	private String lastName;
+	private int GradeYear;
+	private String studentID;
+	private String courses = null ;
+	private int tuitionBalance = 0;
+	private  static int costOfCourse = 600;
+	private static int id = 1000;
+	
+	
+	// constructor: promote user to enter student's name and year
+	
+	public student() {
+		
+		java.util.Scanner in = new Scanner(System.in);		
+		System.out.print("Enter student first name: ");
+		this.firstName = in.nextLine();
+		
+		System.out.print("Enter student Last name: ");
+		this.lastName = in.nextLine();
+				
+		System.out.print("1- Freshmen\n2- Sophomore\n3- Junior\n4- Senior\nEnter student class level: ");
+		this.GradeYear = in.nextInt();
+		
+		setStudentID();
+		
+		System.out.println(firstName+", "+lastName + " "+GradeYear+" "+studentID);
+		
+			}
+	
+	// Generate ID
+	
+	private void setStudentID() {
+		// Grade level + ID
+
+		id++;
+		this.studentID= GradeYear + ""+id;
+		
+	}	
+	
+	// Enroll to courses 	
+	public void enroll() {
+		//Get inside a loop, user hits 0
+		
+		do {
+			System.out.print("Enter course to enroll (Q to quit): ");
+			Scanner in = new Scanner(System.in);
+			String course = in.nextLine();
+			if (!course.equals("Q")) {
+				courses=  courses+"\n"+ course;
+				tuitionBalance = tuitionBalance + costOfCourse;
+			}
+			else {
+				break;
+			}
+		} while (1 !=0);
+		
+		
+		System.out.println("ENROLLED IN: " + courses);
+		System.out.println("TUITION BALANCE: $"+ tuitionBalance);
+		
+	}
+	
+	// View balance 	
+	public void viewBalance() {
+		
+		System.out.println("Your balance is: $"+ tuitionBalance);		
+	}
+	
+	// Pay Tuition
+	
+	public void payTuition() {
+		
+		viewBalance();
+		System.out.println("Enter your payment: $");
+		Scanner in = new Scanner(System.in);
+		int payment = in.nextInt();
+		tuitionBalance = tuitionBalance - payment;
+		System.out.println("Thank you for your payment of $" + payment);
+		
+		viewBalance();
+		
+	}
+	// Show status
+	
+	
+	
+}
---
+++ Student Database Application/src/studentDatabaseApp.java
@@ -0,0 +1,11 @@
+
+public class studentDatabaseApp {
+
+	public static void main(String[] args) {
+		
+		student stu1 = new student();
+		stu1.enroll();
+		stu1.payTuition();
+	}
+
+}
